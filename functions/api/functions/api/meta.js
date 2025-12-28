f40f83bf8ab80f92ec6ce66bf39ed9e9c34b20d2
a563498 <115269813+a563498@users.noreply.github.com>
2025-12-29 07:10:18 +0900

    Delete functions/api directory

--- functions/api/functions/api/meta.js
+++
@@ -1,6 +0,0 @@
-import { jsonResponse, pickDailyAnswer } from './_common.js';
-export async function onRequestGet({ env }){
-  const a = await pickDailyAnswer(env);
-  if (!a.ok) return jsonResponse({ ok:false, message:a.message, detail:a.detail }, a.status||502);
-  return jsonResponse({ ok:true, dateKey:a.dateKey, fixedDaily:true, env:{ hasKey:!!env.OPENDICT_KEY, hasKV:!!env.TTEUTGYOP_KV }});
-}
