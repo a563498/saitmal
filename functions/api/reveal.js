326d34325f10c27768cac1d712d569bafb57bae8
a563498 <115269813+a563498@users.noreply.github.com>
2025-12-29 01:30:31 +0900

    Delete functions/api directory

--- functions/api/reveal.js
+++
@@ -1,9 +0,0 @@
-import { jsonResponse, pickDailyAnswer } from './_common.js';
-export async function onRequestGet({ request, env }) {
-  const url = new URL(request.url);
-  const game = url.searchParams.get("game");
-  const a = await pickDailyAnswer(env, game);
-  if (!a.ok) return jsonResponse({ ok:false, message: a.message, detail: a.detail }, a.status || 500);
-  const ans = a.data;
-  return jsonResponse({ ok:true, word: ans.word, pos: ans.pos || "", def: ans.definition || "" });
-}
